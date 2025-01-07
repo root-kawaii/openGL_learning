b87b19e7e9148bc3e13daafef793728455db1dcb
root-kawaii <teo.regge.99@gmail.com>
2025-01-07 23:28:55 +0100

    init

---
+++ CMakeFiles/hephaestus-openGL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hephaestus-openGL.dir/src/glad.c.o"
+  "CMakeFiles/hephaestus-openGL.dir/src/glad.c.o.d"
+  "CMakeFiles/hephaestus-openGL.dir/src/main.cpp.o"
+  "CMakeFiles/hephaestus-openGL.dir/src/main.cpp.o.d"
+  "hephaestus-openGL"
+  "hephaestus-openGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/hephaestus-openGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
